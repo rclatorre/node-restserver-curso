98a078a552d92b8f3b5b7d6738c2ca6b18586e67
Roberto Carlos Latorre Letona <rclatorre@gmail.com>
2019-11-19 17:04:55 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,65 @@
+//Carga variables y constantes globales
+require('./config/config');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+//------------
+// Middlewares
+//------------
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse application/json
+app.use(bodyParser.json());
+
+//-------------
+// Metodos
+//-------------
+// GET
+app.get('/usuario', function(req, res) {
+    res.json('getUsuario')
+})
+
+// POST
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        })
+
+    }
+
+})
+
+// PUT
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+})
+
+// DELETE
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando en el puerto', process.env.PORT);
+})
